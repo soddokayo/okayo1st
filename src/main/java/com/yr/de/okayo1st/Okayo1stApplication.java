ecd75f1c5737ade12fc6c334130a727daf667e5e
soddokayo <soddokayo@gmail.com>
2021-09-06 21:00:28 +0900

    Initial commit on Github: 1st example by okayo with SpringBoot

---
+++ src/main/java/com/yr/de/okayo1st/Okayo1stApplication.java
@@ -0,0 +1,13 @@
+package com.yr.de.okayo1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Okayo1stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Okayo1stApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yr/de/okayo1st/Okayo1stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yr.de.okayo1st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Okayo1stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
